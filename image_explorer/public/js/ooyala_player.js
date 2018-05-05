c4b57da43aec8c10bc661df3f4c39d11d6ddc234
afzaledx <afzal@opencraft.com>
2018-05-05 22:26:44 +0500

    first commit

---
+++ image_explorer/public/js/ooyala_player.js
@@ -0,0 +1,11 @@
+debugger;
+$('#ooyalaplayer_2').closest('.image-explorer-hotspot').on('feedback:open', function (evt) {
+    debugger;
+    console.log("hello");
+    OO.Player.create('ooyalaplayer_2', 'hiZ3lpeTplH2jtdlbzGNfTmmb0pW5zlW');
+}).on('feedback:close', function (evt) {
+    OO.Player.create('ooyalaplayer_2').pause();
+});
+if (!window.OO) {
+    require(['//player.ooyala.com/static/v4/production/core.js']);
+}
